b7e9767f2c4942862fe10c700fbe10ff3fff2137
Steven Laverty <sdlaverty123@gmail.com>
2023-04-01 17:14:53 -0400

    training configurations

---
+++ scripts/train-superfamily.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+#SBATCH -N 1
+#SBATCH --partition=el8-rpi
+#SBATCH --gres=gpu:4
+#SBATCH -t 06:00:00
+#SBATCH -o train%j.out
+
+. /gpfs/u/scratch/HPDM/shared/miniconda3/etc/profile.d/conda.sh
+conda activate rl-fold
+
+srun train.py "config/single-superfamily.json" 1000 -a -p
