919fc2e291c586e5f35c25a24427945f17eff27b
Steven Laverty <sdlaverty123@gmail.com>
2023-04-03 11:00:12 -0400

    update visualize script

--- scripts/visualize.sh
+++ scripts/visualize.sh
@@ -4,11 +4,11 @@
 #SBATCH --partition=el8-rpi
 #SBATCH --gres=gpu:1
 #SBATCH -t 00:05:00
-#SBATCH -o visualize%j.out
+#SBATCH -o visualize.out
 
 . /gpfs/u/scratch/HPDM/shared/miniconda3/etc/profile.d/conda.sh
 conda activate rl-fold
 
 srun simulate.py -o single-sequence-results.out \
-    deep-q models/single-sequence-q_model_iter_793.pth \
+    deep-q models/single-sequence-q_model_iter_944.pth \
     /gpfs/u/scratch/HPDM/shared/pdb/pdb_mmcif/mmcif_files/2igd.cif A
